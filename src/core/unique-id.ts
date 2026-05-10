cab5097c6af94b62951e74eecce8d60563efe03b
injuk <ingnoh1001@gmail.com>
2026-05-10 16:28:40 +0900

    feat: nanoid 기반 UniqueId 생성 모듈 작성

---
+++ src/core/unique-id.ts
@@ -0,0 +1,13 @@
+import { nanoid } from 'nanoid';
+
+const DEFAULT_LENGTH = 24;
+
+export default {
+    create(): string {
+        return nanoid(DEFAULT_LENGTH);
+    },
+
+    from(length: number): string {
+        return nanoid(length);
+    },
+};
